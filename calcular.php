79c13fc2372813c58d8cde68468be5aeea36cd7b
Ana Saa <ana.saa@utp.ac.pa>
2021-07-08 15:16:48 -0500

    se agrego demas ecuaciones

--- calcular.php
+++ calcular.php
@@ -34,4 +34,30 @@ echo "El monto por flete es: ".$flete."<br>";
 
 echo "El descuento aplicado fue: ".$descuento; 
 
+$edad = 24; 
+
+switch($edad) 
+
+{ 
+
+    case 12: 
+
+        echo "La mitad de tu edad"; 
+
+        break; 
+
+    case 24: 
+
+        echo "Esta es tu edad"; 
+
+        break; 
+
+    case 48: 
+
+        echo "El doble de tu edad"; 
+
+        break; 
+
+} 
+
 ?> 
